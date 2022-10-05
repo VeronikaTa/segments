7c2d02186162ef93bc6e17e97e02a520f64fd568
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-10-05 23:04:22 +0300

    task comple1ted

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -6,20 +6,62 @@ import static java.lang.StrictMath.pow;
 
 class Segment {
 
-    public Segment(Point start, Point end) {
+    Point start;
+    Point end;
 
+    public Segment(Point start, Point end) throws IllegalArgumentException{
+
+        if (start == null && end == null) {
+            throw new IllegalArgumentException();
+        }
+        if (start.getX() == end.getX() && start.getY() == end.getY()) {
+            throw new IllegalArgumentException();
+        }
+        if(start.getX() < end.getX()) {
+            this.start = new Point(start.getX(), start.getY());
+            this.end = new Point(end.getX(), end.getY());
+        }
+        else{
+            this.start = new Point(end.getX(), end.getY());
+            this.end = new Point(start.getX(), start.getY());
+        }
     }
 
     double length() {
-
+        double length = Math.sqrt(Math.pow((this.end.getX() - this.start.getX()), 2.0) + Math.pow((this.end.getY() - this.start.getY()), 2.0));
+        return length;
     }
 
     Point middle() {
-
+        double x = ((this.start.getX() + this.end.getX()) / 2);
+        double y = ((this.start.getY() + this.end.getY()) / 2);
+        Point middle = new Point(x, y);
+        return middle;
     }
 
     Point intersection(Segment another) {
+        if(another == null){
+            throw new IllegalArgumentException();
+        }
+        if(this.start.getX() < another.start.getX() && this.end.getX() < another.start.getX()){
+            return null;
+        }
+        if(another.start.getX() < this.start.getX() && another.end.getX() < this.start.getX()){
+            return null;
+        }
+        double A1 = (this.start.getY() - this.end.getY()) / (this.start.getX() - this.end.getX());
+        double A2 = (another.start.getY() - another.end.getY()) / (another.start.getX() - another.end.getX());
+        if(A1 == A2){
+            return null;
+        }
+        double b1 = this.start.getY() - A1 * this.start.getX();
+        double b2 = another.start.getY() - A2 * another.start.getX();
 
+        double Xa  =  (b2 - b1)/(A1-A2);
+        double Ya = A2 * Xa + b2;
+        if ((Xa < Math.max(this.start.getX(), another.start.getX())) || (Xa > Math.min( this.end.getX(), another.end.getX()))) {
+            return null;
+        }
+        return new Point(Xa, Ya);
     }
-
 }
--- src/test/java/com/epam/rd/autotasks/segments/Main.java
+++ src/test/java/com/epam/rd/autotasks/segments/Main.java
@@ -3,26 +3,27 @@ package com.epam.rd.autotasks.segments;
 public class Main {
     public static void main(String[] args) {
 
-        {
-            double length = new Segment(new Point(0, 0), new Point(3, 4)).length();
-            System.out.println(length);
-        }
 
+//        {
+//            double length = new Segment(new Point(0, 0), new Point(3, 4)).length();
+//            System.out.println(length);
+//        }
+//
         {
-            Segment first = new Segment(new Point(0, 0), new Point(4, 4));
-            Segment second = new Segment(new Point(2, 0), new Point(0, 2));
+            Segment first = new Segment(new Point(2, 5), new Point(0.5, 1.5));
+            Segment second = new Segment(new Point(0, 2), new Point(5, 5));
             Point intersection = first.intersection(second);
 
             System.out.println(intersection.getX());
-            System.out.println(intersection.getY());
+           System.out.println(intersection.getY());
         }
-
-        {
-            Segment first = new Segment(new Point(0, 0), new Point(4, 0));
-            Segment second = new Segment(new Point(2, 1), new Point(1, 2));
-            Point intersection = first.intersection(second);
-
-            System.out.println(intersection == null);
-        }
+//
+//        {
+//            Segment first = new Segment(new Point(0, 0), new Point(4, 0));
+//            Segment second = new Segment(new Point(2, 1), new Point(1, 2));
+//            Point intersection = first.intersection(second);
+//
+//            System.out.println(intersection == null);
+//        }
     }
 }
